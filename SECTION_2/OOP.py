61f64ff7a16bc009035d66686618e01b3325247d
Arantis_jr <arantisjunior@gmail.com>
2025-03-31 10:51:30 +0100

    completed section2

--- SECTION_2/OOP.py
+++ SECTION_2/OOP.py
@@ -3,4 +3,20 @@ class Person:
             self.name = name
             self.name = age
 
-class Student
+class Student(Person):
+      def __init__(self, name, age,student_id,grades):
+            self.name = name
+            self.name = age
+            self.student_id = student_id
+            self.grades = grades
+           
+
+      def calculate_average(grades):
+            average_grade = (grades/5)*100
+            print(f"student average:{average_grade}")
+      def display_details(name, age,student_id,grades):
+            print(f"student_name:{name},student_age:{age},grades:{grades},sudent_id:{student_id}")
+     
+            
+
+student1 = Student.display_details("arantis")
